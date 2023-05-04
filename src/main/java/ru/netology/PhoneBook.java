c7cb151cce6df93033d41a8ce4ba0ee2c0c4e052
Dmitriy_Korchagin <793070407232@ya.ru>
2023-05-05 02:21:08 +0300

    findByName implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -18,7 +18,7 @@ public class PhoneBook {
     }
 
     public String findByName(String name) {
-        return null;
+        return contacts.get(name);
     }
 
     public int getPhoneBookSize() {
