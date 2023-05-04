050dde5a8e0270bd3eac8104d481a522b2c54d2f
Dmitriy_Korchagin <793070407232@ya.ru>
2023-05-05 01:29:14 +0300

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,22 @@
 package ru.netology;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+    private Map<String, String> contacts = new TreeMap<>();
+
+    public int add(String name, String phoneNumber) {
+        return 0;
+    }
+
+    public int getPhoneBookSize() {
+        return contacts.size();
+    }
+
+    public boolean phoneBookIsEmpty() {
+        return contacts.isEmpty();
+    }
+
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,13 @@
+import org.junit.jupiter.api.*;
+import ru.netology.PhoneBook;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Dima","+79307040723" );
+        Assertions.assertEquals(1, phoneBook.getPhoneBookSize());
+        Assertions.assertFalse(phoneBook.phoneBookIsEmpty());
+    }
 }
